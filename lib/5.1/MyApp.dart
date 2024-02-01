e4dcdd9e4acef69dbe16cc1983c749645f3a1a9f
Zimil-Patel <patelzimil2310@gmail.com>
2024-02-01 23:14:57 +0530

    Initial commit

---
+++ lib/5.1/MyApp.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color(0xff7cb342),
+          appBar: AppBar(title: const Text('My App',
+            style: TextStyle(color: Colors.white, fontSize: 20),),
+            backgroundColor: const Color(0xff8bc34a),
+            centerTitle: true,),
+          body: Center(
+            child: Container(
+              height: 300,
+              width: 300,
+              decoration: const BoxDecoration(
+                color: Color(0xff4caf50)
+              ),
+              alignment: Alignment.center,
+              child: Container(
+                alignment: Alignment.center,
+                height: 250,
+                width: 250,
+                decoration: const BoxDecoration(
+                  color: Color(0xffb2ff59)
+                ),
+                child: Text('OOOO', style: TextStyle(fontSize: 120, color: Colors.black.withOpacity(0.50), letterSpacing: -40),),
+              ),
+              ),
+            ),
+          ),
+      ),
+      );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:e_commerce_tasks/5.1/MissionOfRNW.dart';
+import 'package:e_commerce_tasks/5.1/MyApp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MissionOfRNW());
+}
