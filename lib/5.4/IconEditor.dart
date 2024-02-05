fff2c12285a255e8e8404c5fb8a03c7eb7f22804
Zimil-Patel <patelzimil2310@gmail.com>
2024-02-05 12:15:01 +0530

    05-02-2024

--- lib/5.4/IconEditor.dart
+++ lib/5.4/IconEditor.dart
@@ -1,9 +1,5 @@
 import 'package:e_commerce_tasks/main.dart';
-import 'package:e_commerce_tasks/util/iconList.dart';
 import 'package:flutter/material.dart';
-
-
-
 
 class IconEditor extends StatefulWidget {
   const IconEditor({super.key});
@@ -22,61 +18,41 @@ List iconColorList = [
   Colors.grey
 ];
 
-Color defaultColor = iconColorList[0];
+int selectedIcon = 0;
+int selectedColor = 0;
 
-Icon defaultIcon = displayIconList[0]['icon'];
-double displayIconSize = 60;
-double listIconSize = 40;
-
-
-List listIcon = [
-  {'name': 'Exit', 'icon':  Icon(Icons.exit_to_app, size: listIconSize, color: defaultColor,)},
-  {'name': 'Play', 'icon':  Icon(Icons.play_arrow, size: listIconSize, color: defaultColor)},
-  {'name': 'Pause', 'icon':  Icon(Icons.pause, size: listIconSize, color: defaultColor)},
-  {'name': 'Stop', 'icon':  Icon(Icons.stop, size: listIconSize, color: defaultColor)},
-  {'name': 'Close', 'icon':  Icon(Icons.close, size: listIconSize, color: defaultColor)},
-  {'name': 'Delete', 'icon':  Icon(Icons.delete, size: listIconSize, color: defaultColor)},
-  {'name': 'Email', 'icon':  Icon(Icons.email, size: listIconSize, color: defaultColor)},
-  {'name': 'Book', 'icon':  Icon(Icons.book, size: listIconSize, color: defaultColor)},
-  {'name': 'Star', 'icon':  Icon(Icons.star, size: listIconSize, color: defaultColor)},
-];
-
-List displayIconList = [
-  {'name': 'Exit', 'icon':  Icon(Icons.exit_to_app, size: displayIconSize, color: defaultColor,)},
-  {'name': 'Play', 'icon':  Icon(Icons.play_arrow, size: displayIconSize, color: defaultColor)},
-  {'name': 'Pause', 'icon':  Icon(Icons.pause, size: displayIconSize, color: defaultColor)},
-  {'name': 'Stop', 'icon':  Icon(Icons.stop, size: displayIconSize, color: defaultColor)},
-  {'name': 'Close', 'icon':  Icon(Icons.close, size: displayIconSize, color: defaultColor)},
-  {'name': 'Delete', 'icon':  Icon(Icons.delete, size: displayIconSize, color: defaultColor)},
-  {'name': 'Email', 'icon':  Icon(Icons.email, size: displayIconSize, color: defaultColor)},
-  {'name': 'Book', 'icon':  Icon(Icons.book, size: displayIconSize, color: defaultColor)},
-  {'name': 'Star', 'icon':  Icon(Icons.star, size: displayIconSize, color: defaultColor)},
+List currentIconList = [
+  Icons.exit_to_app,
+  Icons.play_arrow,
+  Icons.pause,
+  Icons.stop,
+  Icons.close,
+  Icons.delete,
+  Icons.email,
+  Icons.book,
+  Icons.star,
 ];
 
 class _IconEditorState extends State<IconEditor> {
-
-
   @override
   Widget build(BuildContext context) {
-
-
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: Scaffold(
         backgroundColor: const Color(0xffeef0f2),
-
-        appBar: addAppBar(title: 'Icon Editor', textColor: Colors.black, bgColor: Colors.white, centerTitle: true),
-
+        appBar: addAppBar(
+            title: 'Icon Editor',
+            textColor: Colors.black,
+            bgColor: Colors.white,
+            centerTitle: true),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              displayIcon(icon: defaultIcon, iconColor: defaultColor),
+              displayIcon(),
               addTitle('Select Color'),
-              addRow(currentList: iconColorList, forRow: "Color"),
+              addRow(forRow: "Color"),
               addTitle('Select Icon'),
-              addRow(currentList: listIcon, forRow: "Icon"),
+              addRow(forRow: "Icon"),
             ],
           ),
         ),
@@ -84,32 +60,32 @@ class _IconEditorState extends State<IconEditor> {
     );
   }
 
-
   //main icon dsiplay
-  Widget displayIcon({required Icon icon, required Color iconColor}){
+  Widget displayIcon() {
     return Container(
-      height: 240,
-      width: double.infinity,
-      alignment: Alignment.center,
-      margin: const EdgeInsets.all(18),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 10,
-            spreadRadius: 0,
-            color: Colors.grey.shade300,
-          )
-        ]
-      ),
-      child: icon
-    );
+        height: 240,
+        width: double.infinity,
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(18),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10,
+                spreadRadius: 0,
+                color: Colors.grey.shade300,
+              )
+            ]),
+        child: Icon(
+          currentIconList[selectedIcon],
+          size: 70,
+          color: iconColorList[selectedColor],
+        ));
   }
 
   //title container
-  Widget addTitle(String titleName){
-
+  Widget addTitle(String titleName) {
     return Container(
         height: 50,
         width: double.infinity,
@@ -124,16 +100,18 @@ class _IconEditorState extends State<IconEditor> {
                 spreadRadius: 0,
                 color: Colors.grey.shade300,
               )
-            ]
-        ),
-        child: Text(titleName, style: TextStyle(fontSize: 20, color: Colors.grey.shade600, fontWeight: FontWeight.w500),)
-    );
-
+            ]),
+        child: Text(
+          titleName,
+          style: TextStyle(
+              fontSize: 20,
+              color: Colors.grey.shade600,
+              fontWeight: FontWeight.w500),
+        ));
   }
 
   //color row
-  Widget addRow({required List currentList, required String forRow}){
-
+  Widget addRow({required String forRow}) {
     return Container(
         height: 120,
         width: double.infinity,
@@ -148,54 +126,31 @@ class _IconEditorState extends State<IconEditor> {
                 spreadRadius: 0,
                 color: Colors.grey.shade300,
               )
-            ]
-        ),
+            ]),
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Row(
             children: [
-              if(forRow == "Color")
-                ...List.generate(currentList.length, (index) => addColorBtn(currentColor: currentList[index]))
+              if (forRow == "Color")
+                ...List.generate(
+                    iconColorList.length,
+                    (index) => addColorBtn(
+                        currentColor: iconColorList[index], index: index))
               else
-                ...List.generate(currentList.length, (index) => addIconBtn(currentIcon: currentList[index]['icon'], index: index))
+                ...List.generate(
+                    currentIconList.length, (index) => addIconBtn(index: index))
             ],
           ),
-        )
-    );
-
+        ));
   }
 
-  Widget addColorBtn({required Color currentColor}){
+  Widget addColorBtn({required Color currentColor, required int index}) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: InkWell(
-        onTap: (){
+        onTap: () {
           setState(() {
-            defaultColor = currentColor;
-            displayIconList.clear();
-
-              displayIconList.add({'name': 'Exit', 'icon':  Icon(Icons.exit_to_app, size: displayIconSize, color: defaultColor,)});
-              displayIconList.add({'name': 'Play', 'icon':  Icon(Icons.play_arrow, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Pause', 'icon':  Icon(Icons.pause, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Stop', 'icon':  Icon(Icons.stop, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Close', 'icon':  Icon(Icons.close, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Delete', 'icon':  Icon(Icons.delete, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Email', 'icon':  Icon(Icons.email, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Book', 'icon':  Icon(Icons.book, size: displayIconSize, color: defaultColor)});
-              displayIconList.add({'name': 'Star', 'icon':  Icon(Icons.star, size: displayIconSize, color: defaultColor)});
-
-            listIcon.clear();
-            listIcon.add({'name': 'Exit', 'icon':  Icon(Icons.exit_to_app, size: displayIconSize, color: defaultColor,)});
-            listIcon.add({'name': 'Play', 'icon':  Icon(Icons.play_arrow, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Pause', 'icon':  Icon(Icons.pause, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Stop', 'icon':  Icon(Icons.stop, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Close', 'icon':  Icon(Icons.close, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Delete', 'icon':  Icon(Icons.delete, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Email', 'icon':  Icon(Icons.email, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Book', 'icon':  Icon(Icons.book, size: displayIconSize, color: defaultColor)});
-            listIcon.add({'name': 'Star', 'icon':  Icon(Icons.star, size: displayIconSize, color: defaultColor)});
-            print(defaultColor);
-
+            selectedColor = index;
           });
         },
         borderRadius: BorderRadius.circular(100),
@@ -204,29 +159,28 @@ class _IconEditorState extends State<IconEditor> {
           width: 80,
           child: Container(
             decoration: BoxDecoration(
-              color: currentColor,
-              shape: BoxShape.circle,
+                color: currentColor,
+                shape: BoxShape.circle,
                 boxShadow: [
                   BoxShadow(
                     blurRadius: 10,
                     spreadRadius: 0,
                     color: currentColor,
                   )
-                ]
-            ),
+                ]),
           ),
         ),
       ),
     );
   }
 
-  Widget addIconBtn({required Icon currentIcon, required int index}){
+  Widget addIconBtn({required int index}) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: InkWell(
-        onTap: (){
+        onTap: () {
           setState(() {
-            defaultIcon = displayIconList[index]['icon'];
+            selectedIcon = index;
           });
         },
         borderRadius: BorderRadius.circular(100),
@@ -243,10 +197,12 @@ class _IconEditorState extends State<IconEditor> {
                     spreadRadius: 0,
                     color: Colors.grey.shade300,
                   )
-                ]
+                ]),
+            child: Icon(
+              currentIconList[index],
+              size: 40,
+              color: iconColorList[selectedColor],
             ),
-
-            child: currentIcon,
           ),
         ),
       ),
